e1bdb065f43050706a669032fecdfab65f63b2c7
Chris Lee <pachopa0320@gmail.com>
2017-08-19 23:23:43 -0700

    changed some layout on mainPage

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 $(function () {
   // $('.sidebar.top').hide();
 
-  // $('.openButton').on('click', function() {
-  //   $('.sidebar.top').slideToggle(700);
-  // })
-  $('.openButton').hide();
+  // $('.openButton').hide();
+  // $('#fullpage').hide();
 
-  // $(".openButton").click(function(e) {
-  //     e.preventDefault();
-  //     $(".about-page").toggleClass("toggled");
-  // });
-  //$('.about-page').hide()
   $('.openButton').on('click', function (e) {
     e.preventDefault();
     $('.sidebar.top').slideToggle(700);
@@ -21,10 +14,10 @@ $(function () {
     // //e.preventDefault();
     $('#firstPage').toggle(700)
     $('.sidebar.top').slideToggle(700, function () {
-      
+
     })
   })
-  
+
   $('#fullpage').fullpage({
     sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F', '#348899'],
     sectionSelector: '.vertical-scrolling',
@@ -41,19 +34,31 @@ $(function () {
 
 
   $(".typed").typed({
-    strings: ["<h1 style=font-size:60px;>Chris Lee</h1> <div style=font-size:40px;>I'm A Web Developer</div> <br> <div style=font-size:40px;>Living In Gastown Vancouver</div> <br> <div style=font-size:40px;>I Love <em> Music </em> </div> "],
+    strings: ["<h1 style=font-size:150px;>Chris Lee</h1> "],
     typeSpeed: 70,
     backDelay: 0,
     showCursor: null,
-    startDelay: 1000,
+    startDelay: 2000,
     // loop
     loop: false,
     callback: function () {
-      // this is going to be button effect --> $('.resource').delay(500).fadeIn('slow');
-      $('.openButton').fadeIn()
-      setTimeout(function () {
-        $('.typed-effect + .typed-cursor').css({ 'opacity': '0', 'animation': 'blink' });
-      }, 1300);
+      $(".typed2").typed({
+        strings: ["<div style=font-size:40px;>I'm A Web Developer</div>", "<div style=font-size:40px;>Living In Gastown Vancouver</div>", "<div style=font-size:40px;>I Love <em> Music </em> </div> "],
+        typeSpeed: 70,
+        backDelay: 50,
+        showCursor: null,
+        startDelay: 0,
+        // loop
+        loop: true,
+        callback: function () {
+          // this is going to be button effect --> $('.resource').delay(500).fadeIn('slow');
+          $('.openButton').fadeIn()
+          setTimeout(function () {
+            $('.typed-effect + .typed-cursor').css({ 'opacity': '0', 'animation': 'blink' });
+          }, 1300);
+        }
+      })
     }
   })
+
 })
